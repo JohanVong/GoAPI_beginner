196a194554099d522cc154b907dc2ee6a8b0ceef
Almas <Kairat>
2020-10-21 20:31:59 +0600

    safe commit

--- datasources/mysql/usersdb/users_db.go
+++ datasources/mysql/usersdb/users_db.go
@@ -9,25 +9,18 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// USE LOCAL ENV VARIABLES FOR A GITHUB!!!
 const (
-	mysqlUsersUsername = "root"
-	mysqlUsersPassword = "1"
-	mysqlUsersHost     = "127.0.0.1:3306"
-	mysqlUsersSchema   = "users_db"
-
-	queryCreateUsersTable  = "CREATE TABLE `users_db`.`users` (`id` BIGINT(11) NOT NULL AUTO_INCREMENT, `username` VARCHAR(45) NOT NULL, `firstname` VARCHAR(45) NULL, `lastname` VARCHAR(45) NULL, `email` VARCHAR(45) NOT NULL, `date_created` DATETIME NULL, `status` VARCHAR(45) NULL, `password` VARCHAR(32) NOT NULL, PRIMARY KEY (`id`), UNIQUE INDEX `username_UNIQUE` (`username` ASC), UNIQUE INDEX `email_UNIQUE` (`email` ASC));"
-	queryCreateUsersSchema = "CREATE SCHEMA `users_db` ;"
+	// Never load a production version credentials!!!
+	mysqlUsersUsername = "Your Username"
+	mysqlUsersPassword = "Your Password"
+	mysqlUsersHost     = "Your Host"
+	mysqlUsersSchema   = "Your Schema"
 )
 
 // Client is a mySQL database
 var (
 	Client *sql.DB
 
-	// username = os.Getenv(mysqlUsersUsername)
-	// password = os.Getenv(mysqlUsersPassword)
-	// host     = os.Getenv(mysqlUsersHost)
-	// schema   = os.Getenv(mysqlUsersSchema)
 	username = mysqlUsersUsername
 	password = mysqlUsersPassword
 	host     = mysqlUsersHost
@@ -50,35 +43,4 @@ func init() {
 		panic(err)
 	}
 	log.Println("Database configured successfully")
-}
-
-// CreateUsersDatabase to create database for future users
-func CreateUsersDatabase() {
-	var err error
-
-	schemaStatement, err := Client.Prepare(queryCreateUsersSchema)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	defer schemaStatement.Close()
-
-	tableStatement, err := Client.Prepare(queryCreateUsersTable)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	defer tableStatement.Close()
-
-	_, err = schemaStatement.Exec()
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	_, err = tableStatement.Exec()
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
 }
